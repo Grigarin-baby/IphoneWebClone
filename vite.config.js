17dc2aa03d0cb129495269b3f98c146334b2ff8e
Grigarin Baby <grigarinbabywb@gmail.com>
2025-01-16 09:25:18 -0800

    withoutSentry

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    host: "192.168.1.3",
+    port: 5173,
+  },
+  plugins: [react()],
+});
