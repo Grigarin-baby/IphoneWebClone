93e1270f6f404701a6fcb8197c86a575bd809f79
Grigarin Baby <grigarinbabywb@gmail.com>
2025-01-20 13:37:49 -0800

    Fixed Import Statement

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: "192.168.1.3",
-    port: 5173,
-  },
+ 
   plugins: [react()],
 });
